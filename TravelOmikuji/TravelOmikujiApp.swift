4b5d131923ca6328aebfb373a9188b679cf3cc15
snarfnet <82538502+snarfnet@users.noreply.github.com>
2026-05-12 17:20:19 +0900

    fix: move MobileAds init to App.init, use onAppear for ATT

--- TravelOmikuji/TravelOmikujiApp.swift
+++ TravelOmikuji/TravelOmikujiApp.swift
@@ -4,22 +4,16 @@ import GoogleMobileAds
 
 @main
 struct TravelOmikujiApp: App {
-    @Environment(\.scenePhase) private var scenePhase
-    @State private var attRequested = false
+    init() {
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+    }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .onChange(of: scenePhase) {
-                    if scenePhase == .active && !attRequested {
-                        attRequested = true
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                            ATTrackingManager.requestTrackingAuthorization { _ in
-                                DispatchQueue.main.async {
-                                    GADMobileAds.sharedInstance().start { _ in }
-                                }
-                            }
-                        }
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                        ATTrackingManager.requestTrackingAuthorization { _ in }
                     }
                 }
         }
